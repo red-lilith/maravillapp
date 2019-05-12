53afa3b51e64fd8ba4d89f707c3b75d79b621d8e
Diana GarCo <garcia.diana@correounivalle.edu.co>
2019-05-12 10:53:10 -0500

    urls

--- maravillapp/urls.py
+++ maravillapp/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('', include('apps.usuarios.urls')),
     path('productos/', include('apps.productos.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
